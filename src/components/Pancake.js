f917dd5b6abbfda43a200da204c4dbf03262d2b8
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-20 15:47:11 +0000

    Automatically backed up by Learn

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -15,7 +15,9 @@ class Pancake extends React.Component {
     startInterval();
   }
   // TODO: create a componentWillUnmount() which will clear the interval
-
+componentWillUnmount(){
+  cleanUpInterval();
+}
   updateCounter = () => {
     this.setState({
       timeCooked: this.state.timeCooked + 1
